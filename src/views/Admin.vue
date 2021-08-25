2d3bf21f3caf3574f56d5ff0669b67786d5e33c4
Mentoss <clement.toledano@gmail.com>
2021-08-25 09:57:26 +0200

    admin

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-100 grid grid-cols-2 sm:grid-cols-4 text-center">
+  <div class="bg-gray-100 grid grid-cols-2 sm:grid-cols-4 text-center shadow-lg">
     <div class="m-2 p-5 border-2 border-gray-400 font-bold">
       TOTAL<br />{{ totalCount }}
     </div>
@@ -14,71 +14,50 @@
     </div>
   </div>
 
-  <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
-    <div class="w-full overflow-x-auto">
-      <table class="table-fixed">
-        <thead>
-        <tr
-           class="
-              text-md
-              font-semibold
-              tracking-wide
-              text-left text-gray-900
-              bg-gray-100
-              uppercase
-              border-b
-              border-gray-600
-            "
-        >
-          <th class="px-4 py-3 w-15">Infos</th>
-          <th class="px-4 py-3 w-24">Présence</th>
-          <th class="px-4 py-3">Message</th>
-        </tr>
-        </thead>
-        <tbody class="bg-white">
-        <tr
-          v-for="confirmation in confirmationList"
-          :key="confirmation.id"
-          class="text-gray-700 align-top"
-        >
-          <td class="px-5 py-3 border">
-            <p class="font-semibold text-black">
-              {{ confirmation.firstname }} {{ confirmation.lastname }}
-            </p>
-            <p class="text-xs text-gray-600">
-              {{ confirmation.userEmail }}
-            </p>
-            <p class="text-xs text-gray-600">
-              {{ confirmation.phone }}
-            </p>
-            <p class="text-xs text-gray-600">
-              {{ confirmation.address }}
-            </p>
-          </td>
-          <td class="px-8 py-3 text-sm border">
-            <p>
-              Adulte : <span class="font-semibold">{{ confirmation.adult }}</span>
-            </p>
-            <p>
-              Enfant : <span class="font-semibold">{{ confirmation.children }}</span>
-            </p>
-            <p>
-              Dimanche midi :
-              <span
-                :class="
+  <div class="w-full mb-8 overflow-hidden ">
+    <div
+      v-for="confirmation in confirmationList"
+      :key="confirmation.id"
+      class="text-gray-700 align-top my-4 rounded-lg shadow-lg"
+    >
+      <div class="flex">
+        <div class="px-5 py-3 border">
+          <p class="font-semibold text-black">
+            {{ confirmation.firstname }} {{ confirmation.lastname }}
+          </p>
+          <p class="text-xs text-gray-600">
+            {{ confirmation.userEmail }}
+          </p>
+          <p class="text-xs text-gray-600">
+            {{ confirmation.phone }}
+          </p>
+          <p class="text-xs text-gray-600">
+            {{ confirmation.address }}
+          </p>
+        </div>
+        <div class="px-5 py-3 text-sm border ">
+          <p class="">
+            Adulte : <span class="font-semibold">{{ confirmation.adult }}</span>
+          </p>
+          <p>
+            Enfant : <span class="font-semibold">{{ confirmation.children }}</span>
+          </p>
+          <p>
+            Dimanche midi :
+            <span
+              :class="
                   confirmation.sunday ? 'text-green-700 bg-green-100' : ''
                 "
-                class="px-0.5 py-0.5 font-semibold leading-tight rounded-sm"
-              >
+              class="px-0.5 py-0.5 font-semibold leading-tight rounded-sm"
+            >
               {{ confirmation.sunday ? "présent" : "non" }}
               </span>
-            </p>
-          </td>
-          <td class="px-4 py-3 text-sm border">{{ confirmation.message }}</td>
-        </tr>
-        </tbody>
-      </table>
+          </p>
+        </div>
+      </div>
+      <div class="px-4 py-3 text-sm border">{{ confirmation.message }}</div>
     </div>
+
   </div>
 </template>
 
