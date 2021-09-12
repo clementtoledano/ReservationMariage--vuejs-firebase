fcbd1a660a787c97155c821f4ac63dcd17ca0186
Mentoss <clement.toledano@gmail.com>
2021-09-12 17:27:48 +0200

    Map options

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,16 +3,21 @@
     <l-map
       :zoom="zoom"
       :max-zoom="18"
+      :min-zoom="10"
       :center="center"
+      :zoomAnimation="true"
+      :inertiaMaxSpeed="1"
+      :bounds="bounds"
+      :max-bounds="maxBounds"
       style="height: 500px; width: 100%"
     >
       <l-tile-layer :url="url" :attribution="attribution" />
       <l-marker :lat-lng="marker">
         <l-tooltip :options="{ permanent: true, interactive: true }"
-        ><a href="https://goo.gl/maps/6ngUM3z9ukWG97T78" target="_blank">
-          Salle Georges Campo <br />
-          Rue de la source <br />Villeneuve-les-beziers</a
-        >
+          ><a href="https://goo.gl/maps/6ngUM3z9ukWG97T78" target="_blank">
+            Salle Georges Campo <br />
+            Rue de la source <br />Villeneuve-les-beziers</a
+          >
         </l-tooltip>
       </l-marker>
     </l-map>
@@ -23,7 +28,7 @@
 import { defineComponent, ref } from "vue";
 import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer, LMarker, LTooltip } from "@vue-leaflet/vue-leaflet";
-import { latLng } from "leaflet/dist/leaflet-src.esm";
+import { latLngBounds, latLng } from "leaflet/dist/leaflet-src.esm";
 
 export default defineComponent({
   name: "App",
@@ -41,6 +46,14 @@ export default defineComponent({
       '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     );
     const marker = latLng(43.313632692723075, 3.286379692212575);
+    const bounds = latLngBounds([
+      [43.313632692723075, 3.286379692212575],
+      [43.313632692723075, 3.286379692212575],
+    ]);
+    const maxBounds = latLngBounds([
+      [43.313632692723075, 3.286379692212575],
+      [43.313632692723075, 3.286379692212575],
+    ]);
 
     return {
       zoom,
@@ -48,7 +61,12 @@ export default defineComponent({
       url,
       attribution,
       marker,
+      bounds,
+      maxBounds,
     };
   },
 });
 </script>
+
+function latLngBounds(arg0: number, arg1: number) { throw new Error("Function
+not implemented."); }
