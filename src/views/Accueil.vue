b494fd66e13a6c344ec84b44b5c58cc5e3b8db3b
Mentoss <clement.toledano@gmail.com>
2021-10-28 15:52:51 +0200

    maj

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="">
     <div class="font-serif text-center font-bold mb-10">
-      <p class="text-7xl text-gold mb-5 font-cursive">Manon <br />&<br />Clément</p>
+      <p class="text-7xl text-gold mb-5 font-cursive">
+        Manon <br />&<br />Clément
+      </p>
       <p class="text-xl">30 Octobre 2021</p>
       <p class="text-3xl text-gold">J - {{ jour() }}</p>
     </div>
@@ -10,13 +12,15 @@
       <p class="text-xl mb-3 ml-6 font-serif">15h30 - 16h00</p>
     </div>
     <div class="mb-3">
-      <p class="text-3xl font-cursive font-extrabold">2 / Photos sur parvis de la cathédrale</p>
+      <p class="text-3xl font-cursive font-extrabold">
+        2 / Photos sur le parvis de la cathédrale et Musée Fayet
+      </p>
       <p class="text-xl mb-3 ml-6 font-serif">16h00 - 17h30</p>
     </div>
     <div class="text-xl mb-3">
       <p class="text-3xl font-cursive font-extrabold">2 / Aperitif dinatoire</p>
       <p class="mb-3 ml-6 font-serif">
-        A partir de 17h30 <br>
+        A partir de 17h30 <br />
         Salle George Campo / Stade de football <br />
         Rue de la source / D37 <br />
         34420 Villeneuve-les-Beziers
@@ -32,11 +36,11 @@
 // compte à rebour
 function jour() {
   const date1: number = Date.now();
-  const date2: Date = new Date("Oct 31 00:00:01 2021");
+  const date2: Date = new Date("Oct 30 00:00:01 2021");
   const sec = (Number(date2) - date1) / 1000;
   const n = 24 * 3600;
   if (sec > 0) {
-    return Math.floor(sec / n) + 1;
+    return Math.floor(sec / n)+1;
   }
 }
 </script>
